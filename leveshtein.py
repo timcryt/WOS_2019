493d1fbdcdc6b3accf7aa0d4bf4a7e41039291af
timcryt <34748544+timcryt@users.noreply.github.com>
2019-01-05 15:46:48 +0300

    дз по компьютерной лингвистике с 04.01 расстояние Левенштейна

---
+++ leveshtein.py
@@ -0,0 +1,43 @@
+def levenshtein(s1, s2):
+	l = [[0 for i in range(len(s1) + 1)] for i in range(len(s2) + 1)]
+	for i in range(len(s1) + 1):
+		l[0][i] = i
+	for i in range(len(s2) + 1):
+		l[i][0] = i
+	for i in range(1, len(s1) + 1):
+		for j in range(1, len(s2) + 1):
+			l[j][i] = min(l[j-1][i] + 1, l[j][i-1] + 1, l[j-1][i-1] + 1)
+			if s1[i-1] == s2[j-1]:
+				l[j][i] = min(l[j][i], l[j-1][i-1])
+	return l
+
+def levenshtein_distance(s1, s2):
+	return levenshtein(s1, s2)[len(s2)][len(s1)]
+
+def levenshtein_distance_description(s1, s2):
+	l = levenshtein(s1, s2);
+	s = ""
+	i = len(s1)
+	j = len(s2)
+	while (i > 0 and j > 0):
+		if i == 0:
+			i -= 1
+			s += "D"
+		elif j == 0:
+			j -= 1
+			s += "I"
+		elif s1[i-1] == s2[j-1] and l[j-1][i-1] == l[j][i]:
+			i -= 1
+			j -= 1
+			s += "M"
+		elif l[j-1][i-1] + 1 == l[j][i]:
+			i -= 1
+			j -= 1
+			s += "R"
+		elif l[j-1][i] + 1 == l[i][j]:
+			j -= 1
+			s += "I"
+		else:
+			i -= 1
+			s += "D"
+	return s[::-1]
