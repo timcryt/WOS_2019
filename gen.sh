eacc576bd7e1ac80d76549c9542106e2de45215d
timcryt <34748544+timcryt@users.noreply.github.com>
2019-01-08 19:21:46 +0300

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+xd -d corpus.xz
+mv corpus turgenev.txt
+
 echo -e "import json\nmark=gen_mark_from_file(open('turgenev.txt','r'), 12, 6)\njson.dump(mark,open('table.json','w'))" | cat mark_generator.py /dev/stdin | python3
