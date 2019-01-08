266759afa7fb1303359da42bae1c14b612fc8a87
timcryt <34748544+timcryt@users.noreply.github.com>
2019-01-08 16:15:36 +0300

    Update text_generator.py

--- text_generator.py
+++ text_generator.py
@@ -1,3 +1,4 @@
+from random import random
 def gen_word(mark, word):
     r = random()
     s = 0
@@ -8,13 +9,12 @@ def gen_word(mark, word):
 
 def gen_text(mark, length):
     s = ""
-    r = round(random()*len(text))
-    words = text[r:r+key_size]
+    r = round(random()*len(mark['mark']))
+    words = list(list(mark['mark'].keys())[r])
+    key_size = mark['key_size']
     while len(words) < 1200 or not words[len(words) - 1] in '.!?':
         window = ''.join(words[len(words) - key_size: len(words)])
-        new_words = gen_word(mark, window)
+        new_words = gen_word(mark['mark'], window)
         for new_word in new_words:
             words.append(new_word)
-    return ''.join(words)
-    s = ''.join(words)
-gen_text(mark, 1000)
+    return "".join(words)
