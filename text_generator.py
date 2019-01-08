b77307f6e7de6afa5803e56b4cf9f86f250ec13c
timcryt <34748544+timcryt@users.noreply.github.com>
2019-01-08 17:58:29 +0300

    Bug fixed, features added

--- text_generator.py
+++ text_generator.py
@@ -1,20 +1,37 @@
 from random import random
+from nltk import word_tokenize
+from string import punctuation
 def gen_word(mark, word):
     r = random()
     s = 0
-    for next_word in mark[word]:
-        if s <= r < s + mark[word][next_word]:
+    for next_word in mark['mark'][word]:
+        if s <= r < s + mark['mark'][word][next_word]:
             return next_word
-        s += mark[word][next_word]
+        s += mark['mark'][word][next_word]
 
 def gen_text(mark, length):
+    quantum_type = mark['quantum']
     s = ""
-    r = round(random()*len(mark['mark']))
-    words = list(list(mark['mark'].keys())[r])
+    r = int(random()*len(mark['mark']))
+    if quantum_type == 'word':
+        words = list(mark['mark'].keys())[r].split(' ')
+    else:
+        words = list(list(mark['mark'].keys())[r])    
     key_size = mark['key_size']
-    while len(words) < 1200 or not words[len(words) - 1] in '.!?':
-        window = ''.join(words[len(words) - key_size: len(words)])
-        new_words = gen_word(mark['mark'], window)
-        for new_word in new_words:
-            words.append(new_word)
+    while len(words) < length or not words[len(words) - 1] in '.!?':
+        if quantum_type == 'word':
+            window = ' '.join(words[len(words) - key_size: len(words)])
+            new_words = gen_word(mark, window)
+            for new_word in new_words.split(' '):
+                words.append(new_word)
+        else:
+            window = ''.join(words[len(words) - key_size: len(words)])
+            new_words = gen_word(mark, window)
+            for new_word in new_words:
+                words.append(new_word)
+    if quantum_type == 'word':
+        ret = ' '.join(words)
+        for sgn in punctuation:
+            ret = ret.replace(' ' + sgn, sgn)
+        return ret
     return "".join(words)
